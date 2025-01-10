2f67b7c75b6fe2947166e9260de37cb63dc969f2
ravishkadisanayaka <141713471+ravishkadisanayaka@users.noreply.github.com>
2025-01-10 14:15:12 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,14 +2,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "263717750825"
-global.ownername = "Precious Ayomide👑"
+global.ownernomer = "94742291932"
+global.ownername = "Deshan ravishka👑"
 global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
 
-global.ownernumber = '263717750825'  //creator number
-global.ownername = 'Precious Ayomide👑' //owner name
+global.ownernumber = '94742291932'  //creator number
+global.ownername = 'Deshan Ravishka👑' //owner name
 global.botname = 'PREXZY ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
